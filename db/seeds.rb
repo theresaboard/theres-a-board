6a2684690fb25dcb9af6290c57b6b4f6c47e1772
smokecode <smokecode@gmail.com>
2015-10-29 18:22:38 -0400

    faker and initial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do
+  User.create(name: Faker::Internet.user_name)
+end
+
+user_count = User.count
+
+10.times do
+  date = Date.today + rand(7) + 1
+  time = Time.now + rand(86400)
+  datetime = DateTime.new(date.year, date.month, date.day, time.hour, time.min, time.sec, time.zone)
+  user_id = rand(user_count) + 1
+  Timeslot.create(start: datetime, tutor_id: user_id)
+end
