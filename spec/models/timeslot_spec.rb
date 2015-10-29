6925a75640fe8d92f7e4935f9e5795cfd643ca96
Damian C. Rossney <dcrossney@gmail.com>
2015-10-29 19:00:27 -0400

    Add tests for Timeslot model

--- spec/models/timeslot_spec.rb
+++ spec/models/timeslot_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Timeslot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Timeslot do
+
+  context "validations" do
+    it { is_expected.to validate_presence_of :start }
+    it { is_expected.to validate_presence_of :tutor_id }
+  end
+
+  context "associations" do
+    it { is_expected.to belong_to :tutor}
+    it { is_expected.to have_one :student}
+  end
 end
