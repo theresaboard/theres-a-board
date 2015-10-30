1fcad02e3e596ae6223a1efb19039427f99d6968
smokecode <smokecode@gmail.com>
2015-10-30 18:49:38 -0400

    appointment create route

--- app/controllers/api/timeslots_controller.rb
+++ app/controllers/api/timeslots_controller.rb
@@ -5,7 +5,10 @@ class Api::TimeslotsController < ApplicationController
   end
 
   def create
-    if build_params
+    timeslot = Timeslot.new(start: DateTime.parse(params[:timeslot]))
+    timeslot.tutor_id = current_user.id
+    if timeslot.save
+      binding.pry
       render json: { message: "I am the timeslot controller API and need to be fixed because I am a terrible route" }
     else
       @errors = current_user.errors.full_messages
@@ -25,14 +28,14 @@ class Api::TimeslotsController < ApplicationController
   end
 
   private
-  def build_params
-    params[:timeslots].each do |timeslot|
-      @timeslot = Timeslot.new(start: timeslot[:start])
-      @timeslot.tutor_id = current_user.id
-      return false unless @timeslot.save
-    end
-    true
-  end
+  # def build_params
+  #   params[:timeslots].each do |timeslot|
+  #     @timeslot = Timeslot.new(start: (DateTime.parse(params[:timeslot])))
+  #     @timeslot.tutor_id = current_user.id
+  #     return false unless @timeslot.save
+  #   end
+  #   true
+  # end
 
   def safe_params
     params.permit(:start, :id)
--- public/junk.js
+++ public/junk.js
@@ -1,40 +1,71 @@
-$(function(){
-  $('#calendar').fullCalendar({
-    events: {
-      url: '/api/timeslots',
-      type: 'GET',
-    },
-    error: function() {
-        alert('there was an error while fetching events for calendar!');
-    },
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'month,agendaWeek,agendaDay'
-    },
-    editable: true,
-    eventLimit: true,
-    selectable: true,
-    select: function(start, end, allDay, ev) {
-      //datetime object can be accessed by jquery method _d
-      //start._d
-      console.log(start);
-      console.log(end);
-      console.log(ev.data);
-    },
-    eventClick: function(event, jsEvent, view) {
-      //datetime object can be accessed by jquery method _d
-      //event._d
-          //set the values and open the modal
-    }
+$(document).ready(function(){
+  var CalendarShow = {};
+
+  CalendarShow.Controller = function(){
+    this.view = new CalendarShow.View('#calendar');
+  };
+
+  CalendarShow.Controller.prototype.getNewForm = function(start){
+    $('#new_timeslot_div').css('visibility', 'visible');
+    $('#timeslot_start').val(start._d);
+  };
+  CalendarShow.Controller.prototype.createCalenderEvent = function(){
+    var dateTimeObj = new Date($('#timeslot_start').val());
+    var newEvent = { title: 'booked appointment', start: dateTimeObj };
+    $('#calendar').fullCalendar('renderEvent', newEvent, 'stick');
+  };
+  CalendarShow.Controller.prototype.postTimeslot = function(element){
+    $.ajax({
+      data: $(element).serialize(),
+      type: 'post',
+      url: '/api/timeslots'
+    }).then(function(response){
+      $('#new_timeslot_div').css('visibility', 'hidden');
+      ctrlr.createCalenderEvent();
+    }).fail(function(){
+
+    });
+  };
+
+  CalendarShow.View = function(element){
+    this.setupListeners();
+    this.element = $(element);
+  };
+
+  CalendarShow.View.prototype.setupListeners = function(){
+    $('#new_timeslot_div').on('submit','#new_timeslot_form', function(e){
+      e.preventDefault();
+      ctrlr.postTimeslot(this);
+    });
+  };
+
+  var ctrlr = new CalendarShow.Controller;
+
+  $(function(){
+    $('#calendar').fullCalendar({
+      events: {
+        url: '/api/timeslots',
+        type: 'GET',
+      },
+      error: function() {
+          alert('there was an error while fetching events for calendar!');
+      },
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      editable: true,
+      eventLimit: true,
+      selectable: true,
+      select: function(start, end, allDay, ev) {
+        ctrlr.getNewForm(start);
+      },
+      eventClick: function(event, jsEvent, view) {
+        //datetime object can be accessed by jquery method _d
+        //event._d
+            //set the values and open the modal
+      }
+    });
   });
-  // var myCalendar = $('#my-calendar-id');
-  // myCalendar.fullCalendar();
-  // var myEvent = {
-  //   title:"my new event",
-  //   allDay: true,
-  //   start: new Date(),
-  //   end: new Date()
-  // };
-  // myCalendar.fullCalendar( 'renderEvent', myEvent );
 });
