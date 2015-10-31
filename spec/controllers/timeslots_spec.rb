28ad45f24caf46262996360da789f9a2637809b7
Damian C. Rossney <dcrossney@gmail.com>
2015-10-30 21:04:07 -0400

    Add tests to Timeslots#update
    Rspec tests for timeslots update action.

--- spec/controllers/timeslots_spec.rb
+++ spec/controllers/timeslots_spec.rb
@@ -1,30 +1,10 @@
 require 'rails_helper'
-describe Api::TimeslotsController do
+describe TimeslotsController do
   let(:user) { FactoryGirl.create(:user) }
   let(:timeslot) { FactoryGirl.build(:timeslot).attributes }
-  let(:persisted_timeslot) { FactoryGirl.create(:timeslot) }
 
   before(:each) do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  end
-
-  describe '#create' do
-    it 'should exist' do
-      post :create, { timeslots: [timeslot] }
-      expect(response).to be_success
-    end
-
-    it 'should be able to create a new timeslot' do
-      expect do
-        post :create, { timeslots: [timeslot] }
-      end.to change{Timeslot.count}.by(1)
-    end
-
-    it 'should not be able to create a new timeslot with invalid data' do
-      expect do
-        post :create, { timeslots: [] }
-      end.to change{Timeslot.count}.by(0)
-    end
   end
 
   describe '#update' do
