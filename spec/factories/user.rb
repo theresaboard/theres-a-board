839c21a6855024a1303ca66823e8013e9c809174
Damian C. Rossney <dcrossney@gmail.com>
2015-10-31 15:43:54 -0400

    Revise FactoryGirl user factory
    Add new attributes to user factor, including proper email address for
    development environment.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory(:user, aliases: [:tutor, :student]) do
-    name { Faker::Name.name }
+    name  { Faker::Name.name }
+    email 'mentor.me.dev@gmail.com'
+    uid   { SecureRandom.urlsafe_base64 }
+    image { Faker::Avatar.image(nil, "100x100") }
   end
 end
