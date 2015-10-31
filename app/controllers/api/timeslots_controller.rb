45716361663dac97b5e97ba2ba0f9d801b3f7a32
smokecode <smokecode@gmail.com>
2015-10-30 22:13:32 -0400

    create route and test fixed

--- app/controllers/api/timeslots_controller.rb
+++ app/controllers/api/timeslots_controller.rb
@@ -4,7 +4,9 @@ class Api::TimeslotsController < ApplicationController
   end
 
   def create
-    if build_params
+    timeslot = Timeslot.new(start: params[:timeslot])
+    timeslot.tutor_id = current_user.id
+    if timeslot.save
       render json: { message: "I am the timeslot controller API and need to be fixed because I am a terrible route" }
     else
       @errors = current_user.errors.full_messages
@@ -24,14 +26,14 @@ class Api::TimeslotsController < ApplicationController
   end
 
   private
-  def build_params
-    params[:timeslots].each do |timeslot|
-      @timeslot = Timeslot.new(start: timeslot[:start])
-      @timeslot.tutor_id = current_user.id
-      return false unless @timeslot.save
-    end
-    true
-  end
+  # def build_params
+  #   params[:timeslots].each do |timeslot|
+  #     @timeslot = Timeslot.new(start: timeslot[:timeslot])
+  #     @timeslot.tutor_id = current_user.id
+  #     return false unless @timeslot.save
+  #   end
+  #   true
+  # end
 
   def safe_params
     params.permit(:start, :id)
--- spec/factories/timeslot.rb
+++ spec/factories/timeslot.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
       hour = rand(24)
       half_hour = rand(2) == 1 ? 0 : 30
       datetime = Time.local(date.year, date.month, date.day, hour, half_hour)
-    }
+    }.to_s
 
     factory(:booked_timeslot) do
       student
