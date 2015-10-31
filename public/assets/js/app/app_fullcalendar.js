55645a392238454c5f100b0c1684b69b404577b7
smokecode <smokecode@gmail.com>
2015-10-31 13:03:33 -0400

    modal hide and modal insert for eventClick

--- public/assets/js/app/app_fullcalendar.js
+++ public/assets/js/app/app_fullcalendar.js
@@ -9,87 +9,91 @@
 *  Latest update: Aug 1, 2015
 *
 * ---------------------------------------------------------------------------- */
-$(document).ready(function(){
-  $(function() {
-    $('.fullcalendar-basic').fullCalendar({
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay'
-        },
-        editable: false,
-        defaultView: 'agendaWeek',
-        events: {
-            url: '/api/timeslots',
-            type: 'get'
-        },
-        selectable: true,
-        select: function(start, end, allDay, ev) {
-          ctrlr.getNewForm(ev);
-        },
-        eventClick: function(event, jsEvent, view) {
-        }
-    });
-
-    $('.timepicker').pickatime({
-      interval: 30;
-    });
-
-    $('.datepicker').pickadate({
-      // interval: 30;
-    });
-
-    var CalendarShow = {};
-
-    CalendarShow.Controller = function(){
-      this.view = new CalendarShow.View('#calendar');
-    };
-
-    CalendarShow.Controller.prototype.getNewForm = function(event){
-      $('#modal_remote').modal({ show: true; });
-    };
-
-    CalendarShow.Controller.prototype.createCalenderEvent = function(){
-      var dateTimeObj = new Date($('#timeslot_start').val());
-      var newEvent = { title: 'booked appointment', start: dateTimeObj };
-      $('#calendar').fullCalendar('renderEvent', newEvent, 'stick');
-    };
-
-    CalendarShow.Controller.prototype.postTimeslot = function(element){
-      var time = $('.timepicker').val();
-      var date = $('.datepicker').val();
-      var dateTime = date + " " + time;
-      dateTime = new Date(dateTime)
-      $.ajax({
-        data: {timeslot: dateTime},
-        type: 'POST',
-        url: '/api/timeslots'
-      }).then(function(response){
-        $('#new_timeslot_div').css('visibility', 'hidden');
-        ctrlr.createCalenderEvent();
-      }).fail(function(){
-
-      });
-    };
-
-    CalendarShow.View = function(element){
-      this.setupListeners();
-      this.element = $(element);
-    };
-
-    CalendarShow.View.prototype.setupListeners = function(){
-      $('.modal-footer').on('click','button', function(e){
-        e.preventDefault();
-        if ($('timepicker').val() === ""){
-          alert('please select a time.')
-        }
-        else{
-          ctrlr.postTimeslot(this);
-        };
-      });
-
-    };
-
-    var ctrlr = new CalendarShow.Controller;
+$(function() {
+  $('.fullcalendar-basic').fullCalendar({
+      header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,agendaWeek,agendaDay'
+      },
+      editable: false,
+      defaultView: 'agendaWeek',
+      events: {
+          url: '/api/timeslots',
+          type: 'get'
+      },
+      selectable: true,
+      select: function(start, end, allDay, ev) {
+        ctrlr.getNewForm(ev);
+      },
+      eventClick: function(event, jsEvent, view) {
+        var url = '/timeslots/' + event.id
+        $('#modal_remote').modal({
+            remote: url,
+            show: true
+        });
+      }
   });
+
+  $('.timepicker').pickatime({
+    interval: 30
+  });
+
+  $('.datepicker').pickadate({
+  });
+
+  var CalendarShow = {};
+
+  CalendarShow.Controller = function(){
+    this.view = new CalendarShow.View('#calendar');
+  };
+
+  CalendarShow.Controller.prototype.getNewForm = function(event){
+    $('#modal_new_timeslot').modal({ show: true });
+  };
+
+  CalendarShow.Controller.prototype.createDateTime = function (){
+    var date = $('.timepicker').val();
+    var time = $('.datepicker').val();
+    var dateTime = date + " " + time;
+    return (new Date(dateTime))
+  };
+  CalendarShow.Controller.prototype.createCalenderEvent = function(){
+    var dateTimeObj = ctrlr.createDateTime();
+    var newEvent = { title: 'booked appointment', start: dateTimeObj };
+    $('#calendar').fullCalendar('renderEvent', newEvent, 'stick');
+  };
+
+  CalendarShow.Controller.prototype.postTimeslot = function(element){
+    var dateTime = ctrlr.createDateTime();
+    $.ajax({
+      data: {timeslot: dateTime},
+      type: 'POST',
+      url: '/api/timeslots'
+    }).then(function(response){
+      $('#modal_new_timeslot').modal('toggle');
+      ctrlr.createCalenderEvent();
+    }).fail(function(){
+
+    });
+  };
+
+  CalendarShow.View = function(element){
+    this.setupListeners();
+    this.element = $(element);
+  };
+
+  CalendarShow.View.prototype.setupListeners = function(){
+    $('.modal-footer').on('click','button', function(e){
+      e.preventDefault();
+      if ($('timepicker').val() === ""){
+        alert('please select a time.');
+      }
+      else{
+        ctrlr.postTimeslot(this);
+      };
+    });
+  };
+
+  var ctrlr = new CalendarShow.Controller;
 });
