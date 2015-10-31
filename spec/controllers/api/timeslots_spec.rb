d17ae0404b02265231dbcb7b02b3de3815ca3a14
Damian C. Rossney <dcrossney@gmail.com>
2015-10-30 20:53:54 -0400

    Move Api::Timeslots_spec to api folder

---
+++ spec/controllers/api/timeslots_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+describe Api::TimeslotsController do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:timeslot) { FactoryGirl.build(:timeslot).attributes }
+  let(:persisted_timeslot) { FactoryGirl.create(:timeslot) }
+
+  before(:each) do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+
+  describe '#create' do
+    it 'should exist' do
+      post :create, { timeslots: [timeslot] }
+      expect(response).to be_success
+    end
+
+    it 'should be able to create a new timeslot' do
+      expect do
+        post :create, { timeslots: [timeslot] }
+      end.to change{Timeslot.count}.by(1)
+    end
+
+    it 'should not be able to create a new timeslot with invalid data' do
+      expect do
+        post :create, { timeslots: [] }
+      end.to change{Timeslot.count}.by(0)
+    end
+  end
+
+  describe '#update' do
+    let(:timeslot) { FactoryGirl.create(:timeslot) }
+
+    it 'should update a timeslot with valid data' do
+      expect {
+        post :update, { id: timeslot.id }
+      }.to change { timeslot.reload.student_id }.to(user.id)
+      expect(response).to be_success
+    end
+
+  end
+
+end
