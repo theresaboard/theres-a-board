bea9c4e0e049b4dc714a63f22c570261045fdc4b
Heather Drain <hd.heatherdrain@gmail.com>
2015-11-01 16:02:49 -0500

    Fix factory so time is actually a string as intended

--- spec/factories/timeslot.rb
+++ spec/factories/timeslot.rb
@@ -7,8 +7,8 @@ FactoryGirl.define do
       date = Faker::Date.forward(7) + 1
       hour = rand(24)
       half_hour = rand(2) == 1 ? 0 : 30
-      datetime = Time.local(date.year, date.month, date.day, hour, half_hour)
-    }.to_s
+      datetime = Time.local(date.year, date.month, date.day, hour, half_hour).to_s
+    }
     onsite true
 
     factory(:booked_timeslot) do
