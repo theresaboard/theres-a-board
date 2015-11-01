59693f26eb8a94ed79f95cc8ac8b49ebee239f26
Damian C. Rossney <dcrossney@gmail.com>
2015-11-01 18:45:48 -0500

    Add spec for timeslot mailer cancellation methods
    Add specs to verify the proper issuance of email notifications to
    student and tutor that a session has been canceled.

--- spec/mailers/timeslot_mailer_spec.rb
+++ spec/mailers/timeslot_mailer_spec.rb
@@ -25,4 +25,28 @@ RSpec.describe TimeslotMailer, type: :mailer do
 
   end
 
+  describe "tutor_cancel" do
+    let!(:timeslot) { FactoryGirl.create(:booked_timeslot)}
+    let!(:mail) { TimeslotMailer.tutor_cancel(timeslot, timeslot.student) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Mentor Me Cancellation Notification")
+      expect(mail.to).to eq(["mentor.me.dev@gmail.com"])
+      expect(mail.from).to eq(["mentor.me.dev@gmail.com"])
+    end
+
+  end
+
+  describe "student_cancel" do
+    let!(:timeslot) { FactoryGirl.create(:booked_timeslot)}
+    let!(:mail) { TimeslotMailer.tutor_cancel(timeslot, timeslot.student) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Mentor Me Cancellation Notification")
+      expect(mail.to).to eq(["mentor.me.dev@gmail.com"])
+      expect(mail.from).to eq(["mentor.me.dev@gmail.com"])
+    end
+
+  end
+
 end
