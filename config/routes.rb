6c8dfc1bd5b30ac5d8d50b8965b1435fb8796eea
Damian C. Rossney <dcrossney@gmail.com>
2015-11-01 17:17:17 -0500

    Add named route for canceling a scheduled session
    Add route api/timeslots/:id/cancel to allow either the tutor or student
    to cancel a previously scheduled session.

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   root "dashboard#index"
   get "/auth/auth0/callback" => "auth0#callback"
   get "/auth/failure" => "auth0#failure"
-  get   '/login', :to => 'sessions#new', :as => :login
-  get   '/logout', :to => 'sessions#destroy', :as => :logout
+  get   '/login', to: 'sessions#new', as: :login
+  get   '/logout', to: 'sessions#destroy', as: :logout
 
   resources :timeslots, only: [:update, :show, :destroy]
 
   namespace :api, defaults: {format: :json} do
+    patch 'timeslots/:id/cancel', to: 'timeslots#cancel', as: :cancel
     resources :timeslots, only: [:index, :create, :update]
   end
 end
