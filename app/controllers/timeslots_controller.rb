27ba5d544cf01fb66a971c9f530ee6a25f926978
David Tisdall <david.tisdall@katasan.com>
2015-10-31 20:37:03 -0400

    Tidy up views

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -1,13 +1,12 @@
 class TimeslotsController < SecuredController
 
   def update
-    @timeslot = Timeslot.find_by(id: safe_params[:id])
-    @timeslot.student_id = current_user.id
-    if @timeslot.save
+    timeslot = Timeslot.find_by(id: safe_params[:id])
+    timeslot.student_id = current_user.id
+    if timeslot.save
       render text: 'success'
     else
-      @errors = @timeslot.errors.full_messages
-      render text: @errors.to_s, status: 422
+      render text: timeslot.errors.full_messages.to_s, status: 422
     end
   end
 
