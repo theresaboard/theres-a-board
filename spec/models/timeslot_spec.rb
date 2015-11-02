7ac38023fb94a25fc840adb2e2017bfaec42c121
Damian C. Rossney <dcrossney@gmail.com>
2015-11-02 18:52:06 -0500

    Add spec to tutor/student equality validation
    Add spec to Timeslot model validation to prevent tutor from claiming
    their own timeslot as a student.

--- spec/models/timeslot_spec.rb
+++ spec/models/timeslot_spec.rb
@@ -18,6 +18,15 @@ describe Timeslot do
         .scoped_to(:start)
         .with_message('you are already scheduled for another session at this time')
     }
+    let(:timeslot) { FactoryGirl.create(:timeslot) }
+    it "should not let a tutor claim his or her own timeslot" do
+      expect{
+        timeslot.student_id = timeslot.tutor_id
+        timeslot.save
+        expect(timeslot.errors.full_messages).to include("Student you can't tutor yourself :/")
+      }.not_to change { timeslot.reload.student_id }
+    end
+
   end
 
   context "associations" do
