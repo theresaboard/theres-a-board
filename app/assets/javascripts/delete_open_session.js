3e938583957118107193ab688c2cb5ccb89500e2
Heather Drain <hd.heatherdrain@gmail.com>
2015-11-02 18:58:25 -0500

    Rename files for similar naming scheme

---
+++ app/assets/javascripts/delete_open_session.js
@@ -0,0 +1,46 @@
+var deleteOpenSession = function(timeslot_id){
+  event.preventDefault();
+  $("#modal_remote").modal('hide');
+  swal({
+      title: "Are you sure you wish to cancel?",
+      text: "This will delete your mentoring timeslot.",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#EF5350",
+      confirmButtonText: "Yes, cancel this mentoring timeslot.",
+      cancelButtonText: "No, I've changed my mind.",
+      closeOnConfirm: false,
+      closeOnCancel: false
+  },
+  function(isConfirm){
+    if (isConfirm) {
+      $.ajax({
+          type: "DELETE",
+          url: "/api/timeslots/" + timeslot_id
+        }).done(function(){
+            swal({
+                title: "Deleted!",
+                text: "Your mentoring availabilty has been removed.",
+                confirmButtonColor: "#66BB6A",
+                type: "info"
+            });
+            $('#tutor-cal').fullCalendar( 'refetchEvents' );
+        }).fail(function() {
+            swal({
+              title: "Oops...",
+              text: "Something went wrong!",
+              confirmButtonColor: "#EF5350",
+              type: "error"
+            });
+        });
+    }
+    else {
+      swal({
+        title: "Cancelled",
+        text: "Your availabilty is still scheduled.",
+        confirmButtonColor: "#2196F3",
+        type: "error"
+      });
+    }
+  });
+}
