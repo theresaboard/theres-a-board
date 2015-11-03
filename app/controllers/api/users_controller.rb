abfeb04e878445ae86207e4ecf3e5d2459341089
Damian C. Rossney <dcrossney@gmail.com>
2015-11-03 17:16:14 -0500

    Add Api::Users#update
    Add action to update user profile information.

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,18 @@
+class Api::UsersController < SecuredController
+
+  def update
+    if current_user.update(user_params)
+      render plain: { message: 'success' }
+      current_user.track_event('updated profile')
+    else
+      render plain: { message: 'fail' }
+    end
+  end
+
+  private
+
+    def user_params
+      params.permit(:email_notify, :text_notify, :cellphone)
+    end
+
+end
