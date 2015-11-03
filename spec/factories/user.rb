0cb163edf98c61c68f570381cd080beeb41f25f9
Damian C. Rossney <dcrossney@gmail.com>
2015-11-03 15:51:03 -0500

    Revise spec factories to populate new User fields

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
   factory(:user, aliases: [:tutor, :student]) do
-    name  { Faker::Name.name }
-    email 'notify@theresaboard.com'
-    uid   { SecureRandom.urlsafe_base64 }
-    image { Faker::Avatar.image(nil, "100x100") }
+    name         { Faker::Name.name }
+    email        'notify@theresaboard.com'
+    uid          { SecureRandom.urlsafe_base64 }
+    image        { Faker::Avatar.image(nil, "100x100") }
+    email_notify true
+    text_notify  true
+    cellphone    '5555555555'
   end
 end
