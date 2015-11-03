368bca1d097da2057c24fc6adbc3a98d832e8f24
smokecode <smokecode@gmail.com>
2015-11-02 22:06:02 -0500

    removed scope from auth0 init

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -4,7 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ENV["AUTH0_CLIENT"] || 'IjMvSSE8zdo53tB1QFX5U8EDIxCTzW8Z',
     ENV["AUTH0_SECRET"] || '_khLXS1jrKQfp8r-wLpBVzfni0NF_BGkfLvFDT3pIimXpBFr7SOTR2zx4iBeeyJz',
     'dbc-mentor-me.auth0.com',
-    scope: 'read:org',
     callback_path: "/auth/auth0/callback"
   )
 end
