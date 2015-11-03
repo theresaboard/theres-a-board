e225625e49ec832b64d7e2268e5c52622e3cf9db
Damian C. Rossney <dcrossney@gmail.com>
2015-11-03 07:45:01 -0500

    Add spec for ApplicationHelper#start_long_ordinal

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -9,4 +9,11 @@ describe ApplicationHelper do
     end
   end
 
+  context "#start_long_ordinal" do
+    it "should return a Timeslot#start formatted '%A at %l:%M %p'" do
+      timeslot.start = "03 Nov 2015 03:30"
+      expect(start_long_ordinal(timeslot)).to eq("November 3rd, 2015")
+    end
+  end
+
 end
