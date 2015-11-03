6205c7065100ce09a1d6fb784cfa0cb02613ab0f
Damian C. Rossney <dcrossney@gmail.com>
2015-11-03 10:47:48 -0500

    Improve test coverage for Api::TimeslotsController

---
+++ spec/controllers/api/timeslots_controller_spec.rb
@@ -0,0 +1,94 @@
+require 'rails_helper'
+describe Api::TimeslotsController do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:timeslot_attribs) { FactoryGirl.attributes_for(:timeslot) }
+  let(:persisted_timeslot) { FactoryGirl.create(:timeslot) }
+
+  before(:each) do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+
+  context "#index" do
+    it "is successful" do
+      get :index, format: :json, start: persisted_timeslot.start, end: persisted_timeslot.start + 7
+      expect(response).to be_success
+    end
+  end
+
+  describe '#create' do
+    it 'should exist' do
+      post :create, timeslot_attribs
+    expect(response).to be_success
+    end
+
+    it 'should be able to create a new timeslot' do
+      expect do
+        post :create, timeslot_attribs
+      end.to change{Timeslot.count}.by(1)
+    end
+
+    it 'should not be able to create a new timeslot with invalid data' do
+      expect do
+        post :create, {start: nil}
+      end.to change{Timeslot.count}.by(0)
+    end
+  end
+
+  describe '#update' do
+    let(:timeslot) { FactoryGirl.create(:timeslot) }
+
+    it 'should update a timeslot with valid data' do
+      expect {
+        post :update, id: timeslot.id
+      }.to change { timeslot.reload.student_id }.to(user.id)
+      expect(response).to be_success
+    end
+  end
+
+  describe '#cancel' do
+    let(:timeslot) { FactoryGirl.create(:booked_timeslot) }
+
+    it 'should allow the tutor to cancel a session' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(timeslot.tutor)
+      expect {
+        patch :cancel, id: timeslot.id
+      }.to change { timeslot.reload.student_id }.to(nil)
+      expect(response).to be_success
+    end
+
+    it 'should allow the student to cancel a session' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(timeslot.student)
+      expect {
+        patch :cancel, id: timeslot.id
+      }.to change { timeslot.reload.student_id }.to(nil)
+      expect(response).to be_success
+    end
+
+    it 'should not allow user that is not tutor or student to cancel a session' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      expect {
+        patch :cancel, id: timeslot.id
+      }.not_to change { timeslot.reload.student_id }
+    end
+
+  end
+
+  context '#destroy' do
+    let!(:timeslot) { FactoryGirl.create(:timeslot) }
+
+    it 'should update a timeslot with valid data' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(timeslot.tutor)
+      expect {
+        delete :destroy, id: timeslot.id
+      }.to change{Timeslot.count}.by(-1)
+    end
+
+    it 'should not update a timeslot with valid data' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      expect {
+        delete :destroy, id: timeslot.id
+      }.not_to change{Timeslot.count}
+    end
+  end
+
+end
