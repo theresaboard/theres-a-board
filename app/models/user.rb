2f1d5d3eef679761e627cd4987a5ecbe9cad416a
Damian C. Rossney <dcrossney@gmail.com>
2015-11-03 16:09:03 -0500

    Add custom setter for User cellphone
    Add custom setter to strip non-numeric characters from cellphone string perfor persisting.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ActiveRecord::Base
   validates_presence_of :text_notify
   validates_presence_of :cellphone, message: "must be present for text notifications", if: 'text_notify'
 
+  def cellphone=(cellphone_number)
+    write_attribute(:cellphone, cellphone_number.gsub(/\D/, '')) unless cellphone_number.nil?
+  end
+
   def track_event(event_name, options={})
     attributes = {:event_name => event_name, :email => self.email, :user_id => self.id}
     attributes[:created_at] = options[:created_at] || Time.now.to_i
