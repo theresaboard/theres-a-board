8a9b04c28632147606750a2dc5ddc3ae6a277244
smokecode <smokecode@gmail.com>
2015-11-03 14:33:06 -0500

    model and config create route

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -2,4 +2,22 @@ class SharesController < ActionController::Base
   def index
     @shares = Share.all.order(id: :desc).limit(50)
   end
+
+  def new
+    @share = Share.new
+  end
+
+  def create
+    share = Share.new(share_params)
+    share.user = current_user
+    if share.save
+      render plan: { message: 'success' }
+    else
+      render plain: { message: 'fail' }
+    end
+  end
+
+  def share_params
+    params.require(:share).permit(:url, :title, :description, :type)
+  end
 end
