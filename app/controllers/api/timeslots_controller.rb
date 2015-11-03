f08d818f8a78c32818d9c4671f9a18bca7818949
David Tisdall <david.tisdall@katasan.com>
2015-11-03 17:31:09 -0500

    Add styling for subject feature

--- app/controllers/api/timeslots_controller.rb
+++ app/controllers/api/timeslots_controller.rb
@@ -16,6 +16,7 @@ class Api::TimeslotsController < SecuredController
   def update
     timeslot = Timeslot.find_by(id: params[:id])
     timeslot.student_id = current_user.id
+    timeslot.update_attributes(safe_params)
     timeslot.save!
     render plain: { message: 'success' }
     timeslot.send_tutor_scheduling_email
