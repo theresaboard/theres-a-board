b7d504c3d932892fc2b5c1b9cae0e3798c1eb9a4
Damian C. Rossney <dcrossney@gmail.com>
2015-11-03 15:50:35 -0500

    Refactor User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ActiveRecord::Base
   has_many :bookings, class_name: 'Timeslot', foreign_key: 'student_id'
   has_many :shares
 
-  validates :name, presence: true
+  validates_presence_of :name
+  validates_presence_of :email_notify
+  validates_presence_of :text_notify
+  validates_presence_of :cellphone, message: "must be present for text notifications", if: 'text_notify'
 
   def track_event(event_name, options={})
     attributes = {:event_name => event_name, :email => self.email, :user_id => self.id}
