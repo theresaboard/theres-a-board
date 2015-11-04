6f08f222df5090a4ced836e013799e2591b4b2df
David Tisdall <david.tisdall@katasan.com>
2015-11-03 22:24:06 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103170740) do
+ActiveRecord::Schema.define(version: 20151103192426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,11 +48,14 @@ ActiveRecord::Schema.define(version: 20151103170740) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.string   "uid"
     t.string   "image"
     t.string   "email"
+    t.boolean  "email_notify",            default: true,  null: false
+    t.boolean  "text_notify",             default: false, null: false
+    t.string   "cellphone",    limit: 15
   end
 
 end
