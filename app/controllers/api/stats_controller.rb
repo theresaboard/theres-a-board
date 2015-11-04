326f49d6d73e79c117d102c8fc90c49188d9dad0
smokecode <smokecode@gmail.com>
2015-11-04 16:37:21 -0500

    completed top 3 look up with rank to json

--- app/controllers/api/stats_controller.rb
+++ app/controllers/api/stats_controller.rb
@@ -1,5 +1,6 @@
 class Api::StatsController < SecuredController
   def index
-    @users = User.joins(:timeslots).select('start < ? AND student_id IS NOT NULL', DateTime.now).group(:tutor_id).order('count_start desc').count('start').first(3)
+    top_three = User.joins(:timeslots).select('start < ? AND student_id IS NOT NULL', DateTime.now).group(:tutor_id).order('count_start desc').count('start').first(3)
+    @users = top_three.map.with_index { |tutor, idx| [User.find(tutor[0]), tutor[1], idx+1] }
   end
 end
