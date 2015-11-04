fb97d0f57656307ee23e5371eff7b1bdcbe87189
Heather Drain <hd.heatherdrain@gmail.com>
2015-11-04 17:55:34 -0500

    Add another test for making sure the controller has error messages

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -15,11 +15,16 @@ describe Api::UsersController do
       expect(response).to be_success
     end
 
-    it 'should not update a timeslot with valid data' do
+    it 'should not update a timeslot with invalid data' do
       expect {
         patch :update, id: user.id, email_notify: nil
       }.not_to change { user.reload.email_notify }
       expect(response).not_to be_success
+    end
+
+    it 'should return error messages with invalid data' do
+      patch :update, id: user.id, text_notify: nil, cellphone: nil
+      expect(user.errors.messages).not_to be_empty
     end
   end
 
