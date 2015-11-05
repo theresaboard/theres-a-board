d7050cd3ad40796c26748c33c4da944fb94d2ab1
smokecode <smokecode@gmail.com>
2015-11-05 16:43:23 -0500

    url padding validation

--- app/models/share.rb
+++ app/models/share.rb
@@ -10,6 +10,7 @@ class Share < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :url, :title, :user
+  validate :http_pad, :host_empty
 
   def color
     color_category = self.category
@@ -23,5 +24,16 @@ class Share < ActiveRecord::Base
     else
       "primary"
     end
+  end
+
+  private
+  def http_pad
+    parsed_url = URI.parse(self.url)
+    self.url = "http://#{url}" if parsed_url.scheme.nil?
+  end
+
+  def host_empty
+    parsed_url = URI.parse(url)
+    errors.add("Url can't be blank.") if parsed_url.host.nil?
   end
 end
